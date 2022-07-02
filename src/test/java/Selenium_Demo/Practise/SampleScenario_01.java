c3ae67040bb7e97ffa36d43236fbd23bdf2a21ce
Gopalaswamy.M <external-mgopalaswamy@fr.christianlouboutin.com>
2022-07-02 13:07:36 +0530

    Sample Framework is committed..

---
+++ src/test/java/Selenium_Demo/Practise/SampleScenario_01.java
@@ -0,0 +1,21 @@
+package Selenium_Demo.Practise;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleScenario_01
+{
+	@Test
+	public void TC01()
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\Demo\\Practise\\Driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+		driver.get("https://www.amazon.com/your-account");
+		driver.manage().window().maximize();
+		Assert.assertEquals("Your Account", driver.getTitle());
+		driver.close();
+	}
+}
